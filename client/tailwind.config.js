cea329a2546544f5a81439f5a08144c35f4f00bd
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2025-07-06 14:16:28 +0100

    Create Angular Frontend

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",  // Important: include your components and templates
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
